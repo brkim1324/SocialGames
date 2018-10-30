1becf6ebc38c64c3e6fde5863880b1506e34377a
Brian <brkim1324@gmail.com>
2018-10-30 09:51:33 -0400

    First Sync

---
+++ Assets/DoorManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+
+	public Transform player;
+
+	public Transform doorA;
+	public Transform buttonA;
+
+	public Transform doorB;
+
+	public Transform buttonB;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Vector3.Distance(player.position, buttonA.position) < 5f)
+		{
+			doorA.Translate(0f, 5f, 0f);
+		}
+
+		if (Vector3.Distance(player.position, buttonB.position) < 6f)
+
+		{
+			doorB.Translate(0f, 5f, 0f);
+
+		}
+	}
+}
+
---
+++ Assets/MainCamera.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+
+	// Use this for initialization
+	public Transform player;
+	public Vector3 offset;
+	public Space offsetPosSpace = Space.Self;
+	private bool lookAt = true;
+
+
+	// put this script on your Main Camera, and make sure your Main Camera is already pointed down
+	//declare a public variable of type Transform called "myPlayer"; // make sure to assign in Inspector
+
+	//UPDATE:
+	//// remember... to access the script's transform, write "transform" (with lowercase "t")
+	//move this transform to myPlayer's position;
+	//move this transform up 5 units on the Y axis;
+	//move this transform back 5 units on the Z axis;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	void LateUpdate()
+	{
+		Refresh();
+	}
+
+	public void Refresh()
+	{
+		if (player == null)
+		{
+			Debug.LogWarning("Missing target ref !", this);
+
+			return;
+		}
+
+		// compute position
+		if (offsetPosSpace == Space.Self)
+		{
+			transform.position = player.TransformPoint(offset);
+		}
+		else
+		{
+			transform.position = player.position + offset;
+		}
+
+		// compute rotation
+		if (lookAt)
+		{
+			transform.LookAt(player);
+		}
+		else
+		{
+			transform.rotation = player.rotation;
+		}
+	}
+}
